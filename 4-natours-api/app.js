aa3ddcaf612c7bce7f42997eff5e867ffbc4ad61
Yijie <yijie02281@hotmail.com>
2023-11-20 11:21:51 +0800

    Security changes
    Installed helmet package for security purposes

--- 4-natours-api/app.js
+++ 4-natours-api/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const ErrorController = require("./controllers/errorController");
@@ -10,20 +11,29 @@ const userRouter = require(`./routes/userRoutes`);
 const app = express();
 
 // Global Middleware
+// 14 security headers - refer to https://www.npmjs.com/package/helmet if needed
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limiting requests (Helps to manage Brute Force DOS)
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   message: "Too many requests from this IP, please try again in a minute",
 });
-
 app.use("/api", limiter);
-app.use(express.json());
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Testing middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
